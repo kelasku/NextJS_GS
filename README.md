16700615d3ae27bafe8a6cee81d5e79a288b46b7
Kelasku <git-9900030@vercel.com>
2020-07-01 05:43:19 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# NextJS_GS
+This project is a official getting started from NextJS. NextJS offers Pre-Rendering; Static Exporting; CSS-in-JS; Zero Configuration; Fully Extensible; Ready for Production; SSR; SSG;
+
+## Things learned
+- Create Next.js App
+- SSR
+- SSG
+- SWR
+- Assets, static files and styles in NextJS
+- Pre-rendering and Data Fetching in NextJS
+- Dynamic Routes
+- API Routes
+- Deployment in Vercel
+- Typescript with NextJS
+
+## Project URL
+<a href="https://next-js-gs.now.sh/" target="_blanc">Visualizar site</a>
+
+## Project Images
+<img src="https://i.imgur.com/lTho7Zh.png" width="800">
+<img src="https://i.imgur.com/B5tasfi.png" width="800">
+
+## Instructions
+First, git Clone
+```sh
+git clone https://github.com/alissin4444/NextJS_GS.git
+```
+than, install dependencies
+```sh
+yarn
+```
+
+## LOCAL SERVER 
+For inicialization run
+```sh
+yarn dev
+```
+
+Now you have a server in http://localhost:3000
---
+++ pages/index.tsx
@@ -0,0 +1,51 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
+import { getSortedPostsData } from '../lib/posts'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home({ 
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>The person who makes me feel harry pottah bro</p>
+        <h1 className={utilStyles.headingMd}>Blog</h1>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
